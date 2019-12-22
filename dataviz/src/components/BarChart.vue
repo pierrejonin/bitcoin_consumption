7980d180a993929745b8c8ef5d7f0fc3ca849dac
Pierre JONIN <pierre.jonin@etu.univ-lyon1.fr>
2019-12-22 22:17:33 +0100

    problem load file

--- dataviz/src/components/BarChart.vue
+++ dataviz/src/components/BarChart.vue
@@ -10,33 +10,32 @@ export default {
   mounted() {
     const svgWidth = 500;
     const svgHeight = 300;
-    // const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+    const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-
-    const dataset = [];
-    d3.csv('/data/test.csv').then((data) => {
-      data.forEach((d) => {
-        dataset.push(d.id);
+    // const dataset = [];
+    // d3.csv('/data/test.csv').then((data) => {
+    //   data.forEach((d) => {
+    //     dataset.push(d.id);
+    //   });
+    //   console.log(dataset);
+    const barPadding = 5;
+    const barWidth = (svgWidth / dataset.length);
+    d3.select('svg')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight)
+      .attr('class', 'bar-chart')
+      .selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .attr('y', (d) => svgHeight - d)
+      .attr('height', (d) => d)
+      .attr('width', barWidth - barPadding)
+      .attr('transform', (d, i) => {
+        const translate = [barWidth * i, 0];
+        return `translate(${translate})`;
       });
-      console.log(dataset);
-      const barPadding = 5;
-      const barWidth = (svgWidth / dataset.length);
-      d3.select('svg')
-        .attr('width', svgWidth)
-        .attr('height', svgHeight)
-        .attr('class', 'bar-chart')
-        .selectAll('rect')
-        .data(dataset)
-        .enter()
-        .append('rect')
-        .attr('y', (d) => svgHeight - d)
-        .attr('height', (d) => d)
-        .attr('width', barWidth - barPadding)
-        .attr('transform', (d, i) => {
-          const translate = [barWidth * i, 0];
-          return `translate(${translate})`;
-        });
-    });
+    // });
   },
 };
 
