ab2f0906339d51617caedd6317996a232b3d61ef
sam1905 <sami.cheminal-comte@etu.univ-lyon1.fr>
2020-01-08 17:28:57 +0100

    added tooltips

--- dataviz/src/components/LineChart.vue
+++ dataviz/src/components/LineChart.vue
@@ -40,12 +40,18 @@ export default {
       .append('div')
       .style('opacity', 0)
       .attr('class', 'tooltip')
-      .style('background-color', 'lightblue')
+      .style('background-color', '#e3d0b0')
       .style('border', 'solid')
       .style('border-width', '2px')
       .style('border-radius', '5px')
       .style('padding', '5px');
 
+    svg.append('text')
+      .attr('x', 500)
+      .attr('y', 610)
+      .attr('text-anchor', 'middle')
+      .style('font-size', '12px')
+      .text('Sources : CIA Factbook; Digiconomist');
 
     // Get the data
     d3.csv('https://pierrejonin.github.io/bitcoin_consumption'.concat('/data/bitcoin_electric.csv'), (error, data) => {
@@ -74,7 +80,8 @@ export default {
         .attr('class', 'axis')
         .attr('transform', `translate(0,${height})`)
         .call(d3.axisBottom(x)
-          .tickFormat(d3.timeFormat('%b %Y')))
+          .tickFormat(d3.timeFormat('%b %Y'))
+          .ticks(11))
         .selectAll('text')
         .style('text-anchor', 'end')
         .attr('dx', '-.8em')
@@ -89,10 +96,10 @@ export default {
       // First interest point
       svg.append('circle')
         .attr('id', 'circle1')
-        .attr('cx', x(new Date(2017, 4, 23)))
-        .attr('cy', y(10.048))
+        .attr('cx', x(new Date(2016, 0, 29)))
+        .attr('cy', y(4.7181))
         .attr('r', 5)
-        .attr('fill', '#69b3a2');
+        .attr('fill', '#1A7EF7');
       const mouseover1 = function o() {
         Tooltip
           .style('opacity', 1);
@@ -102,9 +109,10 @@ export default {
       };
       const mousemove1 = function m() {
         Tooltip
-          .html('a')
-          .style('left', `${d3.mouse(this)[0] - 50}px`)
-          .style('top', `${d3.mouse(this)[1]}px`);
+          .html('Le Bitcoin consomme <br> autant qu\'un million <br> de foyers français')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 20}px`)
+          .style('top', `${d3.mouse(this)[1] - 30}px`);
       };
       const mouseleave1 = function l() {
         Tooltip
@@ -121,10 +129,10 @@ export default {
       // Second interest point
       svg.append('circle')
         .attr('id', 'circle2')
-        .attr('cx', x(new Date(2017, 4, 23)))
-        .attr('cy', y(10.048))
+        .attr('cx', x(new Date(2018, 10, 3)))
+        .attr('cy', y(51.673))
         .attr('r', 5)
-        .attr('fill', '#69b3a2');
+        .attr('fill', '#1A7EF7');
       const mouseover2 = function o() {
         Tooltip
           .style('opacity', 1);
@@ -134,9 +142,10 @@ export default {
       };
       const mousemove2 = function m() {
         Tooltip
-          .html('b')
-          .style('left', `${d3.mouse(this)[0] - 50}px`)
-          .style('top', `${d3.mouse(this)[1]}px`);
+          .html('Le Bitcoin consomme <br> maintenant autant <br> que le Bengladesh <br> (+150M d\'habitants)')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 20}px`)
+          .style('top', `${d3.mouse(this)[1] - 50}px`);
       };
       const mouseleave2 = function l() {
         Tooltip
@@ -153,10 +162,10 @@ export default {
       // Third interest point
       svg.append('circle')
         .attr('id', 'circle3')
-        .attr('cx', x(new Date(2017, 4, 23)))
-        .attr('cy', y(10.048))
+        .attr('cx', x(new Date(2017, 6, 14)))
+        .attr('cy', y(14.0462))
         .attr('r', 5)
-        .attr('fill', '#69b3a2');
+        .attr('fill', '#1A7EF7');
       const mouseover3 = function o() {
         Tooltip
           .style('opacity', 1);
@@ -166,9 +175,10 @@ export default {
       };
       const mousemove3 = function m() {
         Tooltip
-          .html('c')
-          .style('left', `${d3.mouse(this)[0] - 30}px`)
-          .style('top', `${d3.mouse(this)[1] + 30}px`);
+          .html('Le Bitcoin consomme <br> plus que la Corée! <br> (du nord)')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 20}px`)
+          .style('top', `${d3.mouse(this)[1] - 30}px`);
       };
       const mouseleave3 = function l() {
         Tooltip
@@ -177,7 +187,7 @@ export default {
           .style('stroke', 'none')
           .style('opacity', 0.8);
       };
-      d3.select('#circle1')
+      d3.select('#circle3')
         .on('mouseover', mouseover3)
         .on('mousemove', mousemove3)
         .on('mouseleave', mouseleave3);
@@ -188,7 +198,7 @@ export default {
         .attr('cx', x(new Date(2019, 8, 22)))
         .attr('cy', y(80.2968))
         .attr('r', 5)
-        .attr('fill', '#69b3a2');
+        .attr('fill', '#1A7EF7');
       const mouseover4 = function o() {
         Tooltip
           .style('opacity', 1);
@@ -213,6 +223,105 @@ export default {
         .on('mouseover', mouseover4)
         .on('mousemove', mousemove4)
         .on('mouseleave', mouseleave4);
+
+      // Fifth interest point
+      svg.append('circle')
+        .attr('id', 'circle5')
+        .attr('cx', x(new Date(2018, 1, 7)))
+        .attr('cy', y(38.4544))
+        .attr('r', 5)
+        .attr('fill', '#1A7EF7');
+      const mouseover5 = function o() {
+        Tooltip
+          .style('opacity', 1);
+        d3.select(this)
+          .style('stroke', 'black')
+          .style('opacity', 1);
+      };
+      const mousemove5 = function m() {
+        Tooltip
+          .html('Eclatement de la <br> bulle de début 2018. <br> Le bitcoin consomme <br> plus que l\'Irak')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 30}px`)
+          .style('top', `${d3.mouse(this)[1] - 50}px`);
+      };
+      const mouseleave5 = function l() {
+        Tooltip
+          .style('opacity', 0);
+        d3.select(this)
+          .style('stroke', 'none')
+          .style('opacity', 0.8);
+      };
+      d3.select('#circle5')
+        .on('mouseover', mouseover5)
+        .on('mousemove', mousemove5)
+        .on('mouseleave', mouseleave5);
+
+      // Sixth interest point
+      svg.append('circle')
+        .attr('id', 'circle6')
+        .attr('cx', x(new Date(2019, 8, 22)))
+        .attr('cy', y(80.2968))
+        .attr('r', 5)
+        .attr('fill', '#1A7EF7');
+      const mouseover6 = function o() {
+        Tooltip
+          .style('opacity', 1);
+        d3.select(this)
+          .style('stroke', 'black')
+          .style('opacity', 1);
+      };
+      const mousemove6 = function m() {
+        Tooltip
+          .html('Maximum de la <br> consommation électrique. <br> Le niveau est équivalent <br> à celui de la Belgique')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 30}px`)
+          .style('top', `${d3.mouse(this)[1] + 120}px`);
+      };
+      const mouseleave6 = function l() {
+        Tooltip
+          .style('opacity', 0);
+        d3.select(this)
+          .style('stroke', 'none')
+          .style('opacity', 0.8);
+      };
+      d3.select('#circle6')
+        .on('mouseover', mouseover6)
+        .on('mousemove', mousemove6)
+        .on('mouseleave', mouseleave6);
+
+      // Seventh interest point
+      svg.append('circle')
+        .attr('id', 'circle7')
+        .attr('cx', x(new Date(2020, 0, 3)))
+        .attr('cy', y(74.3374))
+        .attr('r', 5)
+        .attr('fill', '#1A7EF7');
+      const mouseover7 = function o() {
+        Tooltip
+          .style('opacity', 1);
+        d3.select(this)
+          .style('stroke', 'black')
+          .style('opacity', 1);
+      };
+      const mousemove7 = function m() {
+        Tooltip
+          .html('Consommation actuelle. <br> Elle correspond à celle du Chili. <br> Ou deux fois celle du Qatar. <br> Ou trois fois celle de l\'Irlande')
+          .style('text-align', 'center')
+          .style('left', `${d3.mouse(this)[0] - 30}px`)
+          .style('top', `${d3.mouse(this)[1] + 100}px`);
+      };
+      const mouseleave7 = function l() {
+        Tooltip
+          .style('opacity', 0);
+        d3.select(this)
+          .style('stroke', 'none')
+          .style('opacity', 0.8);
+      };
+      d3.select('#circle7')
+        .on('mouseover', mouseover7)
+        .on('mousemove', mousemove7)
+        .on('mouseleave', mouseleave7);
     });
   },
 };
@@ -222,7 +331,7 @@ body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; }
     .axis { font: 14px sans-serif; }
     .line {
       fill: none;
-      stroke: steelblue;
+      stroke: #f7931a;
       stroke-width: 2px;
     }
 </style>
