deeb31cfbc6c1faaf696bd813b25df1e326d6880
sam1905 <sami.cheminal-comte@etu.univ-lyon1.fr>
2020-01-08 11:09:26 +0100

    route problem

--- dataviz/src/components/LineChart.vue
+++ dataviz/src/components/LineChart.vue
@@ -1,10 +1,3 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-unused-vars */
-<template>
-    <div id="my_dataviz">
-    </div>
-</template>
-
 <script>
 import * as d3 from 'd3';
 
@@ -12,96 +5,81 @@ export default {
   name: 'LineChart',
   mounted() {
     const margin = {
-      top: 10, right: 100, bottom: 30, left: 30,
+      top: 20, right: 20, bottom: 100, left: 50,
     };
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-    const svg = d3.select('#my_dataviz')
-      .append('svg')
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // parse the date / time
+    const parseTime = d3.timeParse('%Y-%m-%dT%H:%M:%S');
+
+    // set the ranges
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    const valueline = d3.line()
+      .x((d) => x(d.DateandTime))
+      .y((d) => y(d.GUESS));
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    const svg = d3.select('body').append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform',
         `translate(${margin.left},${margin.top})`);
 
+    // Get the data
+    d3.csv('../data/data_processing/bitcoin_electric.csv', (error, data) => {
+      if (error) throw error;
 
-    d3.csv('https://pierrejonin.github.io/bitcoin_consumption'.concat('/data/data_processing/out.csv'), (data) => {
-      // List of groups (here I have one group per column)
-      const allGroup = ['Estimation', 'Borne_sup', 'Borne_inf'];
-
-      // add the options to the button
-      d3.select('#selectButton')
-        .selectAll('myOptions')
-        .data(allGroup)
-        .enter()
-        .append('option')
-        .text((d) => d) // text showed in the menu
-        .attr('value', (d) => d); // corresponding value returned by the button
-
-      // A color scale: one color for each group
-      const myColor = d3.scaleOrdinal()
-        .domain(allGroup)
-        .range(['red', 'blue', 'yellow']);
-
-      // Add X axis --> it is a date format
-      const x = d3.scaleLinear()
-        .domain([0, 10])
-        .range([0, width]);
-      svg.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
-
-      // Add Y axis
-      const y = d3.scaleLinear()
-        .domain([0, 20])
-        .range([height, 0]);
-      svg.append('g')
-        .call(d3.axisLeft(y));
-
-      // Initialize line with group a
-      const line = svg
-        .append('g')
-        .append('path')
-        .datum(data)
-        .attr('d', d3.line()
-          .x((d) => x(+d.Periode))
-          .y((d) => y(+d.valueA)))
-        // eslint-disable-next-line no-unused-vars
-        .attr('stroke', (d) => myColor('valueA'))
-        .style('stroke-width', 4)
-        .style('fill', 'none');
-
-      // A function that update the chart
-      function update(selectedGroup) {
-      // Create new data with the selection?
-        const dataFilter = data.map((d) => ({ Periode: d.Periode, value: d[selectedGroup] }));
-
-        // Give these new data to update line
-        line
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr('d', d3.line()
-            .x((d) => x(+d.Periode))
-            .y((d) => y(+d.value)))
-          // eslint-disable-next-line no-unused-vars
-          .attr('stroke', (d) => myColor(selectedGroup));
-      }
-
-      // When the button is changed, run the updateChart function
-      // eslint-disable-next-line no-unused-vars
-      d3.select('#selectButton').on('change', function f(d) {
-        // recover the option that has been chosen
-        const selectedOption = d3.select(this).property('value');
-        // run the updateChart function with this selected option
-        update(selectedOption);
+      // format the data
+      data.forEach((d) => {
+        // eslint-disable-next-line no-param-reassign
+        d.DateandTime = parseTime(d.DateandTime);
+        // eslint-disable-next-line no-param-reassign
+        d.GUESS = +d.GUESS;
       });
-      return -1;
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, (d) => d.DateandTime));
+      y.domain([0, d3.max(data, (d) => d.GUESS)]);
+
+      // Add the valueline path.
+      svg.append('path')
+        .data([data])
+        .attr('class', 'line')
+        .attr('d', valueline);
+
+      // Add the X Axis
+      svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x)
+          .tickFormat(d3.timeFormat('%B/%Y')))
+        .selectAll('text')
+        .style('text-anchor', 'end')
+        .attr('dx', '-.8em')
+        .attr('dy', '.15em')
+        .attr('transform', 'rotate(-65)');
+
+      // Add the Y Axis
+      svg.append('g')
+        .attr('class', 'axis')
+        .call(d3.axisLeft(y));
     });
   },
 };
 </script>
-
 <style>
-
+body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; }
+    .axis { font: 14px sans-serif; }
+    .line {
+      fill: none;
+      stroke: steelblue;
+      stroke-width: 2px;
+    }
 </style>
