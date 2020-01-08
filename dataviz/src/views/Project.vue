f80b7c095c7a481c387216bcc89a99013cd81163
Pierre JONIN <pierre.jonin@etu.univ-lyon1.fr>
2020-01-08 19:42:55 +0100

    index

---
+++ dataviz/src/views/Project.vue
@@ -0,0 +1,36 @@
+<template>
+  <div role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block href="#" v-b-toggle.accordion-1 variant="info">
+            Goal of the Project
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>
+            The goal of the project is to show the energy impact of the Bitcoin.
+          </b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block href="#" v-b-toggle.accordion-2 variant="info">What is Bitcoin</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>...</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProjectView',
+};
+</script>
